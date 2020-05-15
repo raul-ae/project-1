64eae78baf9d264e053a50111964450deac55b94
Rodrigo Rosas <rodrigo.rosasv@gmail.com>
2020-05-14 21:03:39 -0500

    Finished front

--- WikiDrinks_research/front.js
+++ WikiDrinks_research/front.js
@@ -5,3 +5,16 @@ $(document).ready(function(){
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+  $('.dropdown-trigger').dropdown();
+
+
+
+  $(document).ready(function() {
+    $('select').formSelect();
+  });
+  
+  // Wait for page to be ready before applying CSS changes
+  $( document ).ready(function() {
+    $(".dropdown-content.select-dropdown > li span").css("color", "red");
+  });
